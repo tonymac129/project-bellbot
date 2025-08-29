c5d72286d390e259fa384431c4d1cd2d3ef35c08
tonymac129 <tonyforself@gmail.com>
2025-08-29 16:51:38 -0400

    Added images and updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           text="
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel autem deserunt, magnam temporibus eos quis perspiciatis amet! Soluta esse ut deleniti laudantium illo reiciendis delectus error vel ad! Ut, deserunt.
       Soluta in adipisci quisquam delectus eveniet provident recusandae, odio deserunt possimus veniam a ut omnis minima repellat facere velit vel quam? Atque rerum dolores quaerat labore quo porro perferendis quam!"
-          img="/ecs-bell-bot-site/robot.jpg"
+          img="/ecs-bell-bot-site/robot1.png"
           odd={true}
         />
         <Section
@@ -34,14 +34,14 @@ function App() {
           text="
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel autem deserunt, magnam temporibus eos quis perspiciatis amet! Soluta esse ut deleniti laudantium illo reiciendis delectus error vel ad! Ut, deserunt.
       Soluta in adipisci quisquam delectus eveniet provident recusandae, odio deserunt possimus veniam a ut omnis minima repellat facere velit vel quam? Atque rerum dolores quaerat labore quo porro perferendis quam!"
-          img="/ecs-bell-bot-site/robot.jpg"
+          img="/ecs-bell-bot-site/robot2.png"
         />
         <Section
           heading="Fully Autonomous Bell Ringing Robot"
           text="
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel autem deserunt, magnam temporibus eos quis perspiciatis amet! Soluta esse ut deleniti laudantium illo reiciendis delectus error vel ad! Ut, deserunt.
       Soluta in adipisci quisquam delectus eveniet provident recusandae, odio deserunt possimus veniam a ut omnis minima repellat facere velit vel quam? Atque rerum dolores quaerat labore quo porro perferendis quam!"
-          img="/ecs-bell-bot-site/robot.jpg"
+          img="/ecs-bell-bot-site/robot2.png"
           odd={true}
         />
       </div>
